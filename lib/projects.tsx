f78b2d29c183150ebbc0052b193af489325a2e6f
Bautimp <bautistamontesp@gmail.com>
2026-02-26 16:42:13 -0300

    Cambio de colores y logo

--- lib/projects.tsx
+++ lib/projects.tsx
@@ -1,5 +1,5 @@
 // lib/projects.ts
-import { File, Database, ClockFading  } from 'lucide-react';
+import { FileText, ClockFading  } from 'lucide-react';
 
 // --- ICONO PERSONALIZADO: GUÍA PUNTANA ---
 export const GuiaPuntanaLogo = ({ className }: { className?: string }) => (
@@ -30,10 +30,10 @@ export const PROJECTS = [
     id: 'san-luis-tramites',
     name: 'San Luis Tramites',
     description: 'Mi proximo tramite en San Luis.',
-    icon: File , // Usamos iconos de lucide
-    bgColor: 'bg-blue-500/10',
-    textColor: 'text-blue-500',
-    gradient: 'from-blue-500/20 via-slate-50 to-slate-50 dark:from-blue-500/20 dark:via-[#000000] dark:to-[#000000]',
+    icon: FileText , // Usamos iconos de lucide
+    bgColor: 'bg-violet-500/10',
+    textColor: 'text-violet-500',
+    gradient: 'from-violet-500/20 via-slate-50 to-slate-50 dark:from-violet-500/20 dark:via-[#000000] dark:to-[#000000]',
     link: 'https://san-luis-tramites.vercel.app',
     tags: ['Trámite', 'Tiempo']
   },
